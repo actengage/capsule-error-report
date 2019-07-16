146d3a6a5616ae8c08716548fbe9a9d5bd64447d
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2019-07-15 22:08:54 -0600

    Pre-release commit

--- src/Helpers/Functions/lint.js
+++ src/Helpers/Functions/lint.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function lint(data, options) {
     return new Promise((resolve, reject) => {
-        axios.post('lints', data, options)
+        axios.post('lint', data, options)
             .then(response => {
                 resolve(response.data);
             }, e => {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+// import axios from 'axios';
+// axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+
 new Vue({
     render: h => h(App)
 }).$mount('#app');
