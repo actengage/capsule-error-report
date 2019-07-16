2d2f04a72e059c3cbbd02d8be2554f1cdc4d687d
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2019-07-15 22:11:50 -0600

    Pre-release commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-// import axios from 'axios';
-// axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+import axios from 'axios';
+
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 
 new Vue({
     render: h => h(App)
